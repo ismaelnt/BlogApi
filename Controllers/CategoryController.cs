e5b37852df06ab99fc618c4aab980cd948ac1f59
ismaelnt <ismaelnt97@gmail.com>
2022-11-15 14:27:58 -0300

    feat: add standardization for requests

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 using BlogApi.Data;
+using BlogApi.Dtos;
+using BlogApi.Extensions;
 using BlogApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +17,11 @@ public class CategoryController : ControllerBase
         {
             var categories = await context.Categories.AsNoTracking().ToListAsync();
 
-            return Ok(categories);
+            return Ok(new ResultDto<List<Category>>(categories));
         }
-        catch (Exception e)
+        catch (Exception)
         {
-            return StatusCode(500, "05X04 - Falha interna no servidor");
+            return StatusCode(500, new ResultDto<List<Category>>("05X04 - Falha interna no servidor"));
         }
     }
 
@@ -31,61 +33,70 @@ public class CategoryController : ControllerBase
             var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
 
             if (category is null)
-                return NotFound();
+                return NotFound(new ResultDto<Category>("Conteúdo não encontrado"));
 
-            return Ok(category);
+            return Ok(new ResultDto<Category>(category));
         }
-        catch (Exception e)
+        catch (Exception)
         {
-            return StatusCode(500, "05X05 - Falha interna no servidor");
+            return StatusCode(500, new ResultDto<Category>("05X05 - Falha interna no servidor"));
         }
     }
 
     [HttpPost("v1/categories")]
-    public async Task<IActionResult> PostAsync(BlogDataContext context, Category category)
+    public async Task<IActionResult> PostAsync(BlogDataContext context, EditorCategoryDto categoryDto)
     {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultDto<Category>(ModelState.GetErrors()));
+
         try
         {
+            var category = new Category
+            {
+                Name = categoryDto.Name,
+                Slug = categoryDto.Slug.ToLower()
+            };
+
             await context.Categories.AddAsync(category);
             await context.SaveChangesAsync();
 
-            return Created($"v1/categories/{category.Id}", category);
+            return Created($"v1/categories/{category.Id}", new ResultDto<Category>(category));
         }
-        catch (DbUpdateException e)
+        catch (DbUpdateException)
         {
-            return StatusCode(500, "05XE9 - Não foi possível incluir a categoria");
+            return StatusCode(500, new ResultDto<Category>("05XE9 - Não foi possível incluir a categoria"));
         }
-        catch (Exception e)
+        catch (Exception)
         {
-            return StatusCode(500, "05X10 - Falha interna no servidor");
+            return StatusCode(500, new ResultDto<Category>("05X10 - Falha interna no servidor"));
         }
     }
 
     [HttpPut("v1/categories/{id:int}")]
-    public async Task<IActionResult> PutAsync(int id, BlogDataContext context, Category category)
+    public async Task<IActionResult> PutAsync(int id, BlogDataContext context, EditorCategoryDto categoryDto)
     {
         try
         {
-            var categoryById = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
 
-            if (categoryById is null)
-                return NotFound();
+            if (category is null)
+                return NotFound(new ResultDto<Category>("Conteúdo não encontrado"));
 
-            categoryById.Name = category.Name;
-            categoryById.Slug = category.Slug;
+            category.Name = categoryDto.Name;
+            category.Slug = categoryDto.Slug;
 
-            context.Categories.Update(categoryById);
+            context.Categories.Update(category);
             await context.SaveChangesAsync();
 
-            return Ok(category);
+            return Ok(new ResultDto<Category>(category));
         }
-        catch (DbUpdateException e)
+        catch (DbUpdateException)
         {
-            return StatusCode(500, "05XE8 - Não foi possível alterar a categoria");
+            return StatusCode(500, new ResultDto<Category>("05XE8 - Não foi possível alterar a categoria"));
         }
-        catch (Exception e)
+        catch (Exception)
         {
-            return StatusCode(500, "05X11 - Falha interna no servidor");
+            return StatusCode(500, new ResultDto<Category>("05X11 - Falha interna no servidor"));
         }
     }
 
@@ -97,20 +108,20 @@ public class CategoryController : ControllerBase
             var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
 
             if (category is null)
-                return NotFound();
+                return NotFound(new ResultDto<Category>("Conteúdo não encontrado"));
 
             context.Categories.Remove(category);
             await context.SaveChangesAsync();
 
-            return Ok(category);
+            return Ok(new ResultDto<Category>(category));
         }
-        catch (DbUpdateException e)
+        catch (DbUpdateException)
         {
-            return StatusCode(500, "05XE7 - Não foi possível remover a categoria");
+            return StatusCode(500, new ResultDto<Category>("05XE7 - Não foi possível remover a categoria"));
         }
-        catch (Exception e)
+        catch (Exception)
         {
-            return StatusCode(500, "05X12 - Falha interna no servidor");
+            return StatusCode(500, new ResultDto<Category>("05X12 - Falha interna no servidor"));
         }
     }
 }
---
+++ Dtos/ResultDto.cs
@@ -0,0 +1,28 @@
+namespace BlogApi.Dtos;
+
+public class ResultDto<T>
+{
+    public ResultDto(T data, List<string> errors)
+    {
+        Data = data;
+        Errors = errors;
+    }
+
+    public ResultDto(T data)
+    {
+        Data = data;
+    }
+
+    public ResultDto(List<string> errors)
+    {
+        Errors = errors;
+    }
+
+    public ResultDto(string error)
+    {
+        Errors.Add(error);
+    }
+
+    public T Data { get; private set; }
+    public List<string> Errors { get; private set; } = new();
+}
