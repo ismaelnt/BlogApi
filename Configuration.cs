d775171aea90dbfb37b89b1085853c69eba4eac8
ismaelnt <ismaelnt97@gmail.com>
2022-11-18 07:55:34 -0300

    chore: config token

---
+++ Configuration.cs
@@ -0,0 +1,6 @@
+namespace BlogApi;
+
+public static class Configuration
+{
+    public static string JwtKey { get; set; } = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ";
+}
