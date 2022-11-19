09cc93b999229a9e85f40a39508a3a8cdcf42915
ismaelnt <ismaelnt97@gmail.com>
2022-11-19 07:43:49 -0300

    feat: add ApiKeyAttribute.cs

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,7 @@ namespace BlogApi;
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ";
+    public static string JwtKey = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "IlTevUM/z0ey3NwCV/unWg==";
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
+using BlogApi.Attributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApi.Controllers;
 
+[ApiKey]
 [ApiController]
 [Route("")]
 public class HomeController : ControllerBase
