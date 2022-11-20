9f749e2e3b08a88cb7123a08ba0319cbc8ae4b9c
ismaelnt <ismaelnt97@gmail.com>
2022-11-20 08:12:57 -0300

    feat: add filter by categorie method

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,5 +1,6 @@
 using BlogApi.Data;
 using BlogApi.Dtos;
+using BlogApi.Dtos.Posts;
 using BlogApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -42,6 +43,71 @@ public class PostController : ControllerBase
         catch
         {
             return StatusCode(500, new ResultDto<List<Post>>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        try
+        {
+            var post = await _context.Posts
+                .AsNoTracking()
+                .Include(p => p.Author)
+                .ThenInclude(u => u.Roles)
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (post is null)
+                return NotFound(new ResultDto<Post>("Conteúdo não encontrado"));
+
+            return Ok(new ResultDto<Post>(post));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultDto<Post>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/category/{category}")]
+    public async Task<IActionResult> GetByCategoryAsync(
+        [FromRoute] string category,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var count = await _context.Posts.AsNoTracking().CountAsync();
+            var posts = await _context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .Where(x => x.Category.Slug == category)
+                .Select(x => new GetPostsDto()
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+            return Ok(new ResultDto<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultDto<List<Category>>("05X04 - Falha interna no servidor"));
         }
     }
 }
